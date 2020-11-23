21333dde6b4e737e2eabd6acb96f3cd7a4ca560c
Nathan Abram <58815433+nathanabram@users.noreply.github.com>
2020-11-23 09:55:54 -0500

    Add files via upload

--- project-euler-5.js
+++ project-euler-5.js
@@ -21,58 +21,54 @@ function factorsOf(num){
 	}
 	return factorsArray;
 }
-console.log(factorsOf(20));
 
-// Need a function that returns the prime factors of a number:
-// This code is super broken...
-function primeFactorize(nums){
-	let primeFactors = [];
-		for (i =0; i < nums.length; i++){
-			let notPrime = true;
-			for (j=2; j<= nums[i]/2; j++){
-				if (nums[i] % j === 0 ){
-					notPrime = false;}}
-
-		if (!notPrime){
-			console.log(i + " is not a prime! ")
-			primeFactors.push(factorsOf(nums[i]));
-		} else {
-			console.log(i + " is a prime!")
-			primeFactors.push(i)}
-	}
-}
-console.log(primeFactorize([5,20,50]));
 let numbers = [];
 let extras = [];
 let breakk = false;
 
 for (i=1; i<=20; i++){
 	numbers.push(i);
-	console.log(numbers);
-	breakk=false;
-	for (j=2; j<i; j++){
-		if(i%j === 0 && i!=j ){
-			if (!breakk){
-				numbers.splice(numbers.length-1,1);
-				breakk = true;
+	breakk=false; //This will get set to true if the number is not a prime, so that this whole thing goes a bit faster.
+	for (j=2; j<i; j++){ //Starts at 2 and goes to i/2
+		if(i%j === 0 && i!=j ){ // If j is a factor of i
+			if (!breakk){ // and break has not been pulled yet
+				numbers.splice(numbers.length-1,1); // Then, splice the last element of the array (works because at the start of the thing we "pushed" i) 
+				breakk = true; //Set break equal to true, which skipps this code for each j from here on. (Divisible by any j means its not prime, so don't need it in array)
 			}
-			if((i/j)%j === 0){
-				extras.push(j);
-				console.log(extras)
-			}
-
 		}
-
+		if(((i)%(j*j) === 0) && (j != 0)){  // This is the part that is clunky. What I need to do is figure out how to get the extra prime factors that are important out of a given number. These are mostly squares of primes. Maybe I can just look for where there are squares of primes.
+				extras.push(j);
+			} // Now, this bit of code will run well -- whenever something is not a prime, it will be bumped. 
 	}
-	extras.push("f");
 }
+let extraFactorToSearch4 = 1;
+for (i = 1; i < extras.length; i++){
+	extraFactorToSearch4 = i;
+	extras.splice(i-1, 1);
+	if (!extras.includes(extraFactorToSearch4)){
+		numbers.push(extraFactorToSearch4);
+	}
+
+}
+
 let sumProduct = 1;
-console.log(numbers);
 for (i=0; i< numbers.length; i++){
 	sumProduct = sumProduct * numbers[i];
 
 }
-console.log(extras)
+
+for (i =20; i > 0; i --){
+	if (sumProduct%i != 0){ // If the product is not divisible by one of the numbers, 
+		let breaker = false;
+		for (j = 2; j < i/2; j++){
+		if ((sumProduct*j)%i === 0 &&(!breaker)){ // check to see if the sum needs an extra factor of j, starting from the smallest possible j
+			sumProduct=sumProduct*j; // if it is, multiply that factor in
+			breaker = true; // Now, the product is divisible by the number, so no need to keep checking other factors. 
+		}
+
+		}
+	}
+}
 console.log(sumProduct);
 
 
